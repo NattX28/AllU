d8e35e7a07bac93c8a85a763c837c6d066a8f0cf
NattX28 <natawato28@gmail.com>
2026-03-12 23:45:41 +0700

    refactor(middleware): change from closure func to standard pattern in authMiddleware

--- backend/internal/middleware/auth_middleware.go
+++ backend/internal/middleware/auth_middleware.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"os"
 	"strings"
 
 	"github.com/NattX28/AllU/internal/dto"
@@ -9,60 +10,59 @@ import (
 	"github.com/google/uuid"
 )
 
-func AuthMiddleware(secret string) fiber.Handler {
-	return func(c fiber.Ctx) error {
-		authHeader := c.Get("Authorization")
-		if authHeader == "" {
-			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-				"message": "authorization header missing",
-			})
-		}
-
-		// Check format Bearer token
-		parts := strings.SplitN(authHeader, " ", 2)
-		if len(parts) != 2 || parts[0] != "Bearer" {
-			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-				"message": "invalid authorization header",
-			})
-		}
-
-		// Extract real token
-		tokenStr := strings.TrimSpace(parts[1])
-		if tokenStr == "" {
-			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-				"message": "token missing",
-			})
-		}
-
-		// Verify token
-		token, err := jwt.ParseWithClaims(tokenStr, &dto.Claims{}, func(t *jwt.Token) (interface{}, error) {
-			return []byte(secret), nil
+func AuthMiddleware(c fiber.Ctx) error {
+	secret := os.Getenv("JWT_SECRET")
+	authHeader := c.Get("Authorization")
+	if authHeader == "" {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "authorization header missing",
 		})
-		if err != nil || !token.Valid {
-			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-				"message": "invalid token",
-			})
-		}
-
-		// Store claims in context
-		claims := token.Claims.(*dto.Claims)
-
-		// Convert claims.UserID to uuid.UUID
-		uid, err := uuid.Parse(claims.UserID)
-		if err != nil {
-			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-				"message": "invalid user ID format in token",
-			})
-		}
-
-		c.Locals("userID", uid)
-		c.Locals("role", claims.Role)
-		if claims.ProfileID != "" {
-			c.Locals("profileID", uuid.MustParse(claims.ProfileID))
-		}
-
-		return c.Next()
 	}
+
+	// Check format Bearer token
+	parts := strings.SplitN(authHeader, " ", 2)
+	if len(parts) != 2 || parts[0] != "Bearer" {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "invalid authorization header",
+		})
+	}
+
+	// Extract real token
+	tokenStr := strings.TrimSpace(parts[1])
+	if tokenStr == "" {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "token missing",
+		})
+	}
+
+	// Verify token
+	token, err := jwt.ParseWithClaims(tokenStr, &dto.Claims{}, func(t *jwt.Token) (interface{}, error) {
+		return []byte(secret), nil
+	})
+	if err != nil || !token.Valid {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "invalid token",
+		})
+	}
+
+	// Store claims in context
+	claims := token.Claims.(*dto.Claims)
+
+	// Convert claims.UserID to uuid.UUID
+	uid, err := uuid.Parse(claims.UserID)
+	if err != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "invalid user ID format in token",
+		})
+	}
+
+	c.Locals("userID", uid)
+	c.Locals("role", claims.Role)
+	if claims.ProfileID != "" {
+		c.Locals("profileID", uuid.MustParse(claims.ProfileID))
+	}
+
+	return c.Next()
 }
 
 func RequireRole(roles ...string) fiber.Handler {
